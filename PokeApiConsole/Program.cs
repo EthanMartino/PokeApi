b98402cc12c0e445a4fddb0e7e404c5acf1ebc7b
EthanMartino <martino9448@student.cptc.edu>
2020-04-27 15:46:34 -0700

    Created basic Api Client to retrieve pokemon by name

---
+++ PokeApiConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using PokeApiCore;
+using System;
+using System.Threading.Tasks;
+
+namespace PokeApiConsole
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            PokeApiClient client = new PokeApiClient();
+            string result = await client.GetPokemonByName("charizard");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
